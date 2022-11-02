e898d8fa55ded4ae65779703f8f4700e9a7e43b8
nexweb <nexweb1@gmail.com>
2022-11-02 10:04:56 +0900

    Add increment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import * as Vue from 'vue/dist/vue.esm-bundler.js'
 
 const app = Vue.createApp({
   template: `
-    <h1>Hello {{ msg }} </h1>
+    <button v-on:click="increment">Increment</button>
+    <p>{{ count }} </p>
   `,
   data() {
     return {
-      msg: 'world v1'
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1
     }
   }
 })
