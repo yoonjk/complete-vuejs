cc199770b8936ba7b8a264529e23212e6b94810a
nexweb <nexweb1@gmail.com>
2022-11-02 09:54:45 +0900

    Split html and js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import * as Vue from 'vue/dist/vue.esm-bundler.js'
 
 const app = Vue.createApp({
+  template: `
+    <h1>Hello {{ msg }} </h1>
+  `,
   data() {
     return {
-      msg: 'world'
+      msg: 'world v1'
     }
   }
 })
