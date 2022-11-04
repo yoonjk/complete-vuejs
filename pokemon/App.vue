ea7e794c6c679d979fe53b0dbcc6c9a11d995b91
yoonjk <nexweb1@gmail.com>
2022-11-04 11:36:02 +0900

    Reusing Code to Fetch Evolutions

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -3,6 +3,31 @@
     <card 
       v-for="pokemon in pokemons" 
       :key="pokemon.id"
+      @click="fetchEvolutions(pokemon)"
+    >
+      <template v-slot:title>
+        {{ pokemon.name }}
+      </template>
+
+      <template v-slot:content>
+        <img :src="pokemon.sprite"/>
+      </template>
+
+      <template v-slot:description>
+        <div 
+          v-for="type in pokemon.types"
+          :key="type"
+        >
+          {{ type }}
+      </div>
+      </template>
+    </card>
+  </div>
+
+  <div class="cards">
+    <card 
+      v-for="pokemon in evolutions" 
+      :key="pokemon.id"
     >
       <template v-slot:title>
         {{ pokemon.name }}
@@ -28,7 +53,7 @@
 import Card from "./Card.vue"
 
 const api = 'https://pokeapi.co/api/v2/pokemon'
-const ids = [1, 4, 7]
+const IDS = [1, 4, 7]
 
 export default {
   components: {
@@ -36,29 +61,34 @@ export default {
   },
   data() {
     return {
-      pokemons: []
+      pokemons: [],
+      evolutions: []
     }
   },
-  created() {
-    this.fetchData()
+  async created() {
+    this.pokemons = await this.fetchData(IDS)
   },
 
   methods: {
-    async fetchData() {
+    async fetchEvolutions(pokemon) {
+      console.log('fetch')
+      this.evolutions = await this.fetchData(
+        [pokemon.id + 1, pokemon.id + 2]
+      )
+    },
+    async fetchData(ids) {
       const responses = await Promise.all(
         ids.map(id => window.fetch(`${api}/${id}`))
       )
       const json = await Promise.all(
         responses.map(data => data.json())
       )
-      this.pokemons = json.map(datum => ({
+      return json.map(datum => ({
         id: datum.id,
         name: datum.name, 
         sprite: datum.sprites.other['official-artwork'].front_default,
         types: datum.types.map(type => type.type.name)
       }))
-
-      console.log(this.pokemons)
     }
   }
 }
