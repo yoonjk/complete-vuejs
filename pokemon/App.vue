60a9b4578d23f3e7e0b4156bba3d99f649b10df3
yoonjk <nexweb1@gmail.com>
2022-11-04 12:16:47 +0900

    Separating Business Logic and Presentation

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -1,65 +1,24 @@
 <template>
-  <div class="cards">
-    <card 
-      v-for="pokemon in pokemons" 
-      :key="pokemon.id"
-      @click="fetchEvolutions(pokemon)"
-      :class="{ opace: pokemon.id !== selectedId }"
-      class="card"
-    >
-      <template v-slot:title>
-        {{ pokemon.name }} #{{ pokemon.id }}
-      </template>
+  <pokemon-cards 
+    :pokemons="pokemons"
+    @chosen="fetchEvolutions"
+    :selectedId="selectedId"
+  />
 
-      <template v-slot:content>
-        <img :src="pokemon.sprite"/>
-      </template>
-
-      <template v-slot:description>
-        <div 
-          v-for="type in pokemon.types"
-          :key="type"
-        >
-          {{ type }}
-      </div>
-      </template>
-    </card>
-  </div>
-
-  <div class="cards">
-    <card 
-      v-for="pokemon in evolutions" 
-      :key="pokemon.id"
-    >
-      <template v-slot:title>
-        {{ pokemon.name }} #{{ pokemon.id }}
-      </template>
-
-      <template v-slot:content>
-        <img :src="pokemon.sprite"/>
-      </template>
-
-      <template v-slot:description>
-        <div 
-          v-for="type in pokemon.types"
-          :key="type"
-        >
-          {{ type }}
-      </div>
-      </template>
-    </card>
-  </div>
+  <pokemon-cards 
+    :pokemons="evolutions"
+  />
 </template>
 
 <script>
-import Card from "./Card.vue"
+import PokemonCards from "./PokemonCards.vue"
 
 const api = 'https://pokeapi.co/api/v2/pokemon'
 const IDS = [1, 4, 7]
 
 export default {
   components: {
-    Card
+    PokemonCards
   },
   data() {
     return {
@@ -99,18 +58,6 @@ export default {
 </script>
 
 <style scoped>
-.cards {
-  display: flex
-}
-.opace {
-  opacity: 0.5;
-}
 
-.card:hover {
-  opacity: 1.0;
-}
-img {
-  width: 100%;
-}
 
 </style>
