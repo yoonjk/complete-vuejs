1b28c3d13a978fa7393606c510f9ef860b38b818
yoonjk <nexweb1@gmail.com>
2022-11-04 11:05:21 +0900

    Modular Components

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -1,34 +1,42 @@
 <template>
   <div class="cards">
-    <div 
-      class="card"
-      v-for="p in pokemon"
-      :key="p.id"
+    <card 
+      v-for="pokemon in pokemons" 
+      :key="pokemon.id"
     >
-      <div class="title">
-        {{ p.name }}
-      </div>
-      <div 
-        class="content">
-        <img :src="p.sprite">
-      </div>
-      <div class="description">
-        <div v-for="type in p.types" :key="type">
+      <template v-slot:title>
+        {{ pokemon.name }}
+      </template>
+
+      <template v-slot:content>
+        <img :src="pokemon.sprite"/>
+      </template>
+
+      <template v-slot:description>
+        <div 
+          v-for="type in pokemon.types"
+          :key="type"
+        >
           {{ type }}
-        </div>
       </div>
-    </div>
+      </template>
+    </card>
   </div>
 </template>
 
 <script>
+import Card from "./Card.vue"
+
 const api = 'https://pokeapi.co/api/v2/pokemon'
 const ids = [1, 4, 7]
 
 export default {
+  components: {
+    Card
+  },
   data() {
     return {
-      pokemon: []
+      pokemons: []
     }
   },
   created() {
@@ -43,14 +51,14 @@ export default {
       const json = await Promise.all(
         responses.map(data => data.json())
       )
-      this.pokemon = json.map(datum => ({
+      this.pokemons = json.map(datum => ({
         id: datum.id,
         name: datum.name, 
         sprite: datum.sprites.other['official-artwork'].front_default,
         types: datum.types.map(type => type.type.name)
       }))
 
-      console.log(this.pokemon)
+      console.log(this.pokemons)
     }
   }
 }
@@ -58,34 +66,11 @@ export default {
 
 <style scoped>
 .cards {
-  display: flex;
+  display: flex
 }
 
 img {
   width: 100%;
 }
-.card {
-  border: 1px solid silver;
-  border-radius: 8px;
-  max-width: 200px;
-  margin: 0 5px;
-  cursor: pointer;
-  box-shadow: 0px 1px 3px darkgrey;
-  transition: 0.2s;
-}
-.title, .content, .description {
-  padding: 16px;
-  text-transform: capitalize;
-  text-align: center;
-}
-.title, .content {
-  border-bottom: 1px solid silver;
-}
-.title {
-  font-size: 1.25em;
-}
-.card:hover {
-  transition: 0.2s;
-  box-shadow: 0px 1px 9px darkgrey;
-}
+
 </style>
